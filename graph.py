8aa1c0d4accb60218c521c1c620299cb5339cc28
sami-bre <samuelbirhanu121@gmail.com>
2023-04-04 17:52:02 +0300

    initial commit. now we have the autline of the methods in the graph class in the graph.py file. contributors are supposed to implement the methods they're assignmed to without touching other parts of the code.

---
+++ graph.py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self):
+        pass
+
+    def addNode(self, node: str):
+        pass
+
+    def addEdge(self, node: str, to: str):
+        pass
+
+    def addUndirectedEdge(self, node:str, other:str):
+        pass
+
+    def deleteEdge(self, node: str, to:str):
+        pass
+
+    def deleteUndirected(self, node: str, other:str):
+        pass
+
+    def deleteNode(self, node:str):
+        pass
+
+    def dfs(self, start: str, target: str):
+        pass
+
+    def bfs(self, start: str, target: str):
+        pass
+
+    def ucs(self, start: str, target: str):
+        pass
+
+    def iterativeDeepeningSearch(self, start: str, target: str):  # not sure about this signature. check it out
+        pass
+
+    def bidirectionalSearch(self, start: str, target: str): # not sure about this signature. check it out
+        pass
+
+    def greedySearch(self, start: str, target: str, heuristic: function):
+        pass
+
+    def aStarSearch(self, start: str, target: str, heuristic: function):
+        pass
+
