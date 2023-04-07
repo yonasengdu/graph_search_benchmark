6e6fbf900fdb933077177884f3569e09ff117a55
sami-bre <samuelbirhanu121@gmail.com>
2023-04-07 08:19:01 +0300

    fixed some bugs on the test file. we were displaying (printing) the wrong graph in some places

--- test_graph.py
+++ test_graph.py
@@ -124,7 +124,7 @@ romania.addNode("Pitesti")
 romania.addNode("Bucharest")
 \n""")
       
-graph.show()
+romania.show()
 
 romania.addUndirectedEdge("Arad", "Zerind", 75, 75)
 romania.addUndirectedEdge("Arad", "Timisoara", 118, 118)
@@ -140,16 +140,16 @@ romania.addUndirectedEdge("Fagaras", "Bucharest", 211, 211)
 
 print("""\n
 after:
-graph.addUndirectedEdge("Arad", "Zerind", 75, 75)
-graph.addUndirectedEdge("Arad", "Timisoara", 118, 118)
-graph.addUndirectedEdge("Zerind", "Oradea", 71, 71)
-graph.addUndirectedEdge("Arad", "Sibiu", 140, 140)
-graph.addUndirectedEdge("Oradea", "Sibiu", 151, 151)
-graph.addUndirectedEdge("Sibiu", "Fagaras", 99, 99)
-graph.addUndirectedEdge("Sibiu", "Rimnicu Vilcea", 80, 80)
-graph.addUndirectedEdge("Rimnicu Vilcea", "Pitesti", 97, 97)
-graph.addUndirectedEdge("Pitesti", "Bucharest", 101, 101)
-graph.addUndirectedEdge("Fagaras", "Bucharest", 211, 211)
+romania.addUndirectedEdge("Arad", "Zerind", 75, 75)
+romania.addUndirectedEdge("Arad", "Timisoara", 118, 118)
+romania.addUndirectedEdge("Zerind", "Oradea", 71, 71)
+romania.addUndirectedEdge("Arad", "Sibiu", 140, 140)
+romania.addUndirectedEdge("Oradea", "Sibiu", 151, 151)
+romania.addUndirectedEdge("Sibiu", "Fagaras", 99, 99)
+romania.addUndirectedEdge("Sibiu", "Rimnicu Vilcea", 80, 80)
+romania.addUndirectedEdge("Rimnicu Vilcea", "Pitesti", 97, 97)
+romania.addUndirectedEdge("Pitesti", "Bucharest", 101, 101)
+romania.addUndirectedEdge("Fagaras", "Bucharest", 211, 211)
 \n""")
 
 
